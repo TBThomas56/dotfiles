e88afd8db7f5ea4a483a514368c7da29d561538d
Thomas Binu Thomas <thomas.thomas@diamond.ac.uk>
2025-02-21 16:06:05 +0000

    Added Mappings and Snacks.nvim support

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,14 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- Lazy Plugin Manager mappings
+map("n", "<leader>l", "<cmd> Lazy <cr>")
+map("n", "<leader>ls", "<cmd> Lazy sync <cr>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Overall Motions and Saves
+map("n", "<leader>sv", "<cmd> vsp <cr>")
+map("n", "<leader>s", "<cmd> sp <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>dw", "<cmd> q <cr>")
+map("n", "<leader>dd", "<cmd> qa <cr>")
+map("n", "<leader>E", "<cmd> NvimTreeToggle <cr>")

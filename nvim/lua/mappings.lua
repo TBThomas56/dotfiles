82c3409975c403b68287cad167ced8cba3277d1b
Thomas Binu Thomas <thomas.thomas@diamond.ac.uk>
2025-02-21 16:56:55 +0000

    Added Lazygit Integration - non-Snack Version

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,12 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 -- Lazy Plugin Manager mappings
-map("n", "<leader>l", "<cmd> Lazy <cr>")
+map("n", "<leader>ll", "<cmd> Lazy <cr>")
 map("n", "<leader>ls", "<cmd> Lazy sync <cr>")
 
 -- Overall Motions and Saves
 map("n", "<leader>sv", "<cmd> vsp <cr>")
-map("n", "<leader>s", "<cmd> sp <cr>")
+map("n", "<leader>ss", "<cmd> sp <cr>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>dw", "<cmd> q <cr>")
 map("n", "<leader>dd", "<cmd> qa <cr>")

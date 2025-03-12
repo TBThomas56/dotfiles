564283ad1111c200dfc4aaa5b1c660bd8bd512ec
Thomas Binu Thomas <thomas.thomas@diamond.ac.uk>
2025-03-12 01:12:40 +0000

    Improved LSP, Formatter, Noice & To-do Plugins

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,20 +1,46 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
--- Default Servers 
-local servers = { "html", "cssls", "bashls", "clangd", "denols", "gopls", "helm_ls", "lua_ls", "marksman", "pyright", "ruff", "rust_analyzer", "ts_ls", "dockerls","yamlls", "jsonls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+-- Default Servers
+local servers = {
+	"html",
+	"cssls",
+	"bashls",
+	"clangd",
+	"gopls",
+	"helm_ls",
+	"lua_ls",
+	"marksman",
+	"pyright",
+	"ruff",
+	"rust_analyzer",
+	"ts_ls",
+	"dockerls",
+	"jsonls",
+	"yamlls",
+}
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
+
+lspconfig.helm_ls.setup({
+	settings = {
+		["helm-ls"] = {
+			yamlls = {
+				path = "yaml-language-server",
+			},
+		},
+	},
+})
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
